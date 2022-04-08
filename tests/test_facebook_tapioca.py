54a64773a874d053d365af0d6270401ded748560
ilindrey <scronullik@fastmail.com>
2022-04-08 23:05:12 +1100

    corrected tests

--- tests/test_facebook_tapioca.py
+++ tests/test_facebook_tapioca.py
@@ -1,21 +1,14 @@
-# coding: utf-8
 
 import unittest
 
-from tapioca_facebook import Facebook
+from aiotapioca_facebook import Facebook
 
 
-class TestTapioca(unittest.TestCase):
+def test_resource_access():
 
-    def setUp(self):
-        self.wrapper = Facebook(client_id='client_id', access_token='access_token')
+    wrapper = Facebook(client_id='client_id', access_token='access_token')
 
-    def test_resource_access(self):
-        resource = self.wrapper.user_feed(id='me')
+    resource = wrapper.user_feed(id='me')
 
-        self.assertEqual(resource.data(), 'https://graph.facebook.com/me/feed')
+    assert resource.data == 'https://graph.facebook.com/me/feed'
 
-
-
-if __name__ == '__main__':
-    unittest.main()
